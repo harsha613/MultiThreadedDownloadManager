0a1fa9834ab8a07ff07e86f2694380a9a3e54e53
harsha613 <harsha@Harsha.localdomain>
2026-07-19 12:31:04 +0000

    parsing metadata to fill file info

--- download_manager/downloader.py
+++ download_manager/downloader.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import requests
 
+from download_manager.utils import FileInfo
+
 class Downloader:
 
     CHUNK_SIZE = 8192
@@ -21,3 +23,31 @@ class Downloader:
                     f.write(chunk)
 
         print(f"Downloaded completed: {self.output_path}")
+
+    def get_file_info(self) -> FileInfo:
+        """
+        Retrieve file metadata without downloading the entire file.
+        """
+        response = requests.head(self.url, allow_redirects=True)
+        response.raise_for_status()
+        headers = response.headers
+
+        file_size = int(headers.get("Content-Length", 0))
+
+        content_type = headers.get(
+            "Content-Type",
+            "application/octet-stream"
+        )
+
+        supports_ranges = (
+            headers.get("Accept-Ranges", "none").lower() == "bytes"
+        )
+
+        filename = self.output_path.name
+
+        return FileInfo(
+            filename=filename,
+            file_size=file_size,
+            content_type=content_type,
+            supports_ranges=supports_ranges
+        )
