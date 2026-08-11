86cca71fa782de8ebb41243ded75d0a66a3425db
harsha613 <harsha@Harsha.localdomain>
2026-08-11 12:24:39 +0000

    modified downloader to download the chunks in multiple threads and merge the chunks

--- download_manager/downloader.py
+++ download_manager/downloader.py
@@ -1,37 +1,71 @@
 from pathlib import Path
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 from download_manager.utils import FileInfo
 from download_manager.progress import ProgressTracker
+from download_manager.chunk import create_chunks
+from download_manager.worker import download_chunk
+from download_manager.merger import merge_chunks
 
 class Downloader:
 
     CHUNK_SIZE = 8192
 
-    def __init__(self, url: str, output_path: Path):
+    def __init__(
+        self,
+        url: str,
+        output_path: Path,
+        num_threads: int = 4,
+        ):
         self.url = url
         self.output_path = output_path
+        self.num_threads = num_threads
 
     def download(self) -> None:
         """
-        Download the file to the specified output path
+        Download the file to specified output path,
+        using multiple threads if the server supports range requests.
         """
         file_info = self.get_file_info()
 
-        tracker = ProgressTracker(file_info.file_size)
+        print(f"File: {file_info.filename}")
+        print(f"Size: {file_info.file_size} bytes")
 
-        response = requests.get(self.url, stream=True)
+        chunks = create_chunks(
+            file_info.file_size,
+            self.num_threads,
+        )
 
-        response.raise_for_status()
+        print(f"Created {len(chunks)} chunks for download.")
 
-        with self.output_path.open("wb") as f:
-            for chunk in response.iter_content(chunk_size=self.CHUNK_SIZE):
-                if chunk:
-                    f.write(chunk)
-                    tracker.update(len(chunk))
+        chunk_paths = [
+            self.output_path.with_suffix(
+                f"{self.output_path.suffix}.part{chunk.index}"
+            )
+            for chunk in chunks
+        ]
 
-        tracker.finish()
-        print(f"Downloaded completed: {self.output_path}")
+        with ThreadPoolExecutor(
+            max_workers=self.num_threads
+        ) as executor:
+
+            futures = []
+
+            for chunk, chunk_path in zip(chunks, chunk_paths):
+                future = executor.submit(
+                    download_chunk,
+                    self.url,
+                    chunk,
+                    str(chunk_path),
+                )
+                futures.append(future)
+
+            for future in futures:
+                future.result()  # Even if one of the downloads fails, this will raise an exception
+
+        merge_chunks(chunk_paths, self.output_path,)
+        print(f"Download completed and merged into {self.output_path}")
 
     def get_file_info(self) -> FileInfo:
         """
